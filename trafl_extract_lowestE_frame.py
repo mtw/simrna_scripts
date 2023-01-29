1d30947ec420e4ad58bcf11eaf793a246823e617
Michael T. Wolfinger <michael@wolfinger.eu>
2023-01-29 20:47:34 +0100

    make Python3-aware

--- trafl_extract_lowestE_frame.py
+++ trafl_extract_lowestE_frame.py
@@ -1,19 +1,20 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
-import sys, os
+import sys
+import os
 
 if(len(sys.argv) < 2):
-    print >>sys.stderr, "usage: trafl_finds_lowestE_frame.py file.trafl"
+    print ("usage: trafl_finds_lowestE_frame.py file.trafl", file=sys.stderr)
     sys.exit(1)
 
 inpfilename = sys.argv[1]
 
 if(inpfilename[-6:]!=".trafl"):
-    print >>sys.stderr, "extension of input file: "+inpfilename+" has to be .trafl"
+    print ("extension of input file: "+inpfilename+" has to be .trafl", file=sys.stderr)
     sys.exit(1)
 
-if(os.path.exists(inpfilename) == False):
-    print >>sys.stderr, "specified file: "+inpfilename+" doesn't exist"
+if(os.path.exists(inpfilename) is False):
+    print ("specified file: "+inpfilename+" doesn't exist", file=sys.stderr)
     sys.exit(1)
 
 counter = 1
@@ -26,29 +27,28 @@ curr_line = inpfile.readline()
 while(curr_line):
 
     if(len(curr_line) < 100):
-	splitted_line = curr_line.split()
-	curr_energy = float(splitted_line[-2])
+        splitted_line = curr_line.split()
+        curr_energy = float(splitted_line[-2])
 
-	if(lowest_energy > curr_energy):
+        if(lowest_energy > curr_energy):
+            lowest_energy = curr_energy
+            lowest_frame = counter
+            lowestE_header = curr_line
+            lowestE_coords = inpfile.readline()
 
-	    lowest_energy = curr_energy
-	    lowest_frame = counter
-	
-	    lowestE_header = curr_line
-	    lowestE_coords = inpfile.readline()
-	
-	counter += 1
-
+        counter += 1
     curr_line = inpfile.readline()
 
 inpfile.close()
 
-print lowest_frame, lowest_energy
+print(lowest_frame, lowest_energy)
 
 outfilename = inpfilename.replace(".trafl","_minE.trafl")
-print outfilename
+print(outfilename)
 
-outfile = open(outfilename,"w")
-print >>outfile, lowestE_header,
-print >>outfile, lowestE_coords,
-outfile.close()
+original_stdout = sys.stdout
+with open(outfilename, 'w') as f:
+    sys.stdout = f
+    print (lowestE_header)
+    print (lowestE_coords)
+    sys.stdout = original_stdout
